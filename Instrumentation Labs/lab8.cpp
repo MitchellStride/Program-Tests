18d7a16f172b1ce30b35ecc9cfaf3478e27a76a2
Mitchell Stride <mitchellcstride@gmail.com>
2018-11-06 11:25:58 -0330

    Added working HMI test

---
+++ Instrumentation Labs/lab8.cpp
@@ -0,0 +1,58 @@
+/**********************
+ * Flite Text to Speach Program
+ * *******************/
+
+#include <iostream>
+#include "flite.h"
+using namespace std;
+
+//{flite_text_to_speech(const char *text, cst_voice *voice, const char *outtype);}
+
+void sayVersion();
+void fliteVersion();
+
+int main(){
+    //sayVersion();
+    fliteVersion();
+    return 0;
+}  
+
+void fliteVersion(){
+    char c;
+    cout << "Input a character.... \n t: Current Time\n n: Group Names \n q: Quit  \n";
+    
+    while(1){
+        cin >> c;
+        if(c == 't'){
+            system("date | say\n");
+        }
+        if(c == 'n'){
+            system("say Mitchell Stride, Jordan Mayo, Ian Correa.\n");
+        }
+        if(c == 'q'){
+            cout << "Exit\n";
+            system("say Exiting Program\n");
+            return;
+        }
+    }
+}
+
+void sayVersion(){
+    char c;
+    cout << "Input a character.... \n t: Current Time\n n: Group Names \n q: Quit  \n";
+    
+    while(1){
+        cin >> c;
+        if(c == 't'){
+            system("date | say\n");
+        }
+        if(c == 'n'){
+            system("say Mitchell Stride, Jordan Mayo, Ian Correa.\n");
+        }
+        if(c == 'q'){
+            cout << "Exit\n";
+            system("say Exiting Program\n");
+            return;
+        }
+    }
+}
